f9cb9b891b3544018c5e819fe6d83fed4f5db0c8
Thalia Guedes <thalia.guedes17@gmail.com>
2024-07-22 13:15:11 -0300

    chore: alias configuration

--- srv/main/routes/purchase-order-management.ts
+++ srv/main/routes/purchase-order-management.ts
@@ -1,11 +1,9 @@
-import { Request, Service } from "@sap/cds";
+import { Request, Service } from '@sap/cds';
+
+import { db } from '../../common/entities/db/models';
 
 export default (service: Service) => {
-  service.after(
-    "READ",
-    "PurchaseOrderHeaders",
-    (results: any[], request: Request) => {
-      results.forEach((poHeader) => (poHeader.company = "ABC"));
-    }
-  );
+    service.after('READ', 'PurchaseOrderHeaders', (results: db.models.PurchaseOrderHeaders[], request: Request) => {
+        results.forEach(poHeader => poHeader.company = 'ABC');
+    });
 };
