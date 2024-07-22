8a1edca3c7934ada04faf6eac75b83f34f86ce6d
Thalia Guedes <thalia.guedes17@gmail.com>
2024-07-22 13:25:50 -0300

    feat: exposure of entities in the service

--- srv/main/routes/purchase-order-management.ts
+++ srv/main/routes/purchase-order-management.ts
@@ -1,9 +1,30 @@
 import { Request, Service } from '@sap/cds';
 
-import { db } from '../../common/entities/db/models';
+import '../configuration/module-alias';
+
+import { db } from '@/common/entities/db/models';
 
 export default (service: Service) => {
     service.after('READ', 'PurchaseOrderHeaders', (results: db.models.PurchaseOrderHeaders[], request: Request) => {
         results.forEach(poHeader => poHeader.company = 'ABC');
+    });
+    service.before('CREATE', 'PurchaseOrderHeaders', (request: Request) => {
+        const items = [
+            {
+                material: 'Material_insomnia',
+                quantity: 10,
+                uom: 'PC',
+                netPrice: 30,
+                center: 'ABCD'
+            },
+            {
+                material: 'Material_insomnia2',
+                quantity: 5,
+                uom: 'KG',
+                netPrice: 500,
+                center: 'ABCD'
+            }
+        ];
+        request.data.items = items;
     });
 };
